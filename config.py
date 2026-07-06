4dc41bdedeb6ac5eaa2d27c1ec1046f6a09e4c73
Swaranjali Zore <297390573+swaranjalizore@users.noreply.github.com>
2026-07-06 17:16:09 +0530

    Added environment configuration

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+# Load variables from .env file
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    DB_HOST = os.getenv("DB_HOST")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_NAME = os.getenv("DB_NAME")
